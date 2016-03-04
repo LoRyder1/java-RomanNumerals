2a053c4e9c3ae062f855d30c2433974c48d0875e
Lovinder Pnag <lspnag@gmail.com>
2016-03-04 17:56:53 -0500

    first test passing for converting arabic

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,3 +1,13 @@
 
 public class RomanNumerals {
+
+    private final int arabic;
+
+    public RomanNumerals(int arabic) {
+        this.arabic = arabic;
+    }
+
+    public String convertArabic() {
+        return "I";
+    }
 }
--- test/RomanNumeralsTest.java
+++ test/RomanNumeralsTest.java
@@ -1,8 +1,20 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class RomanNumeralsTest {
 
     private RomanNumerals number;
 
     @Test
+    public void whenRomanNumeralsIsPassedOneAStringIisReturned() {
+        number = new RomanNumerals(1);
+        assertEquals("I", number.convertArabic());
+    }
+
+    @Test
+    public void whenRomanNumeralsIsPassedTwoAStringIIisReturned() {
+        number = new RomanNumerals(2);
+        assertEquals("II", number.convertArabic());
+    }
 }
