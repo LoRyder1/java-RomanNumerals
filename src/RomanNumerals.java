a9f0419372df6b660f86e7de1594f49348d23467
Lovinder Pnag <lspnag@gmail.com>
2016-03-05 21:33:02 -0500

    convert Roman is now adding up arabic num

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,14 +5,14 @@ public class RomanNumerals {
     private final int arabic;
     private final String roman;
 
-    private NavigableMap<Integer, String> navMap;
+    private NavigableMap<Integer, String> navDescMap;
 
     public RomanNumerals(int arabic, String roman) {
         this.arabic = arabic;
         this.roman = roman;
 
 //      The outer pair of braces means that you are declaring and initializing an anonymous inner class that extends TreeMap. The inner pair of braces represents an instance initializer, code that is run when an instance is created.
-        navMap = new TreeMap<Integer, String>() {{
+        navDescMap = new TreeMap<Integer, String>() {{
             put(1000, "M");
             put(900, "CM");
             put(500, "D");
@@ -26,16 +26,17 @@ public class RomanNumerals {
             put(5, "V");
             put(4, "IV");
             put(1, "I");
-        }};
+        }}.descendingMap();
     }
 
     public String convertArabic() {
 
         String roman = "";
         int x = arabic;
-        NavigableMap<Integer, String> desMap = navMap.descendingMap();
+//        NavigableMap<Integer, String> desMap = navMap.descendingMap();
+//        maybe use descendingKeySet
 
-        for(Map.Entry<Integer, String> entry : desMap.entrySet()) {
+        for(Map.Entry<Integer, String> entry : navDescMap.entrySet()) {
 
             int repeat = x/entry.getKey();
             for(int i = 0; i < repeat; i++) {
@@ -48,24 +49,14 @@ public class RomanNumerals {
 
     public int convertRoman() {
         String x = roman;
+        int arabic = 0;
 
-        for (Map.Entry<Integer, String> entry : navMap.entrySet()) {
-            System.out.print(entry.getKey() + ": ");
+        for (Map.Entry<Integer, String> entry : navDescMap.entrySet()) {
+            while (x.startsWith(entry.getValue())) {
+                arabic += entry.getKey();
+                x = x.substring(entry.getValue().length());
+            }
         }
-
-
-        return 1;
+        return arabic;
     }
 }
-
-
-//        def convert_numeral input
-//            @roman = input
-//            @arabic = 0
-//            ROMAN_NUMERALS.values.each do |roman|
-//                while input.start_with?(roman)
-//                    @arabic += ROMAN_NUMERALS.invert[roman]
-//                    input = input.slice(roman.length, input.length)
-//                end
-//            end
-//        end
